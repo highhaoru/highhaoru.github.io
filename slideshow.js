b64a03b49811e6d8960b1ac3559dea311e0ef018
highhaoru <jin.ha@northeastern.edu>
2023-09-01 06:11:36 -0400

    notes modified

--- slideshow.js
+++ slideshow.js
@@ -1,15 +1,10 @@
-// 定义一个变量来跟踪当前显示的幻灯片的索引
 let slideIndex = 1;
-
-// 初始化幻灯片，显示第一张
 showSlides(slideIndex);
 
-// 创建一个函数来增加或减少当前显示的幻灯片的索引
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// 创建一个函数来显示当前索引的幻灯片
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
