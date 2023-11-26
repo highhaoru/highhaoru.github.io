bbbaa12077ae85d67d32bd5700a24e34c22999ac
highhaoru <jin.ha@northeastern.edu>
2023-11-25 21:37:05 -0500

    main funcs of musicbox page is all set

--- musicbox/Audio.js
+++ musicbox/Audio.js
@@ -1,19 +1,20 @@
-var Audio = {
-	gainNode: undefined,
-	bufferList: undefined,
-	audioContext: new (window.AudioContext ||
-	                   window.webkitAudioContext)(),
-	init: function(bufferList) {
-		this.bufferList = bufferList;
-		this.gainNode = this.audioContext.createGain();
-		this.gainNode.gain.value = 1;
-		this.gainNode.connect(this.audioContext.destination);
-	},
-	play: function(i) {
-		var sound = this.audioContext.createBufferSource();
-		sound.connect(this.gainNode);
-		sound.buffer = this.bufferList[i];
-		sound.start(0);
-		sound.stop(this.audioContext.currentTime + 18);
-	}
+const Audio = {
+    gainNode: undefined,
+    bufferList: undefined,
+    audioContext: new (window.AudioContext || window.webkitAudioContext)(),
+
+    init(bufferList) {
+        this.bufferList = bufferList;
+        this.gainNode = this.audioContext.createGain();
+        this.gainNode.gain.value = 1;
+        this.gainNode.connect(this.audioContext.destination);
+    },
+
+    play(i) {
+        const sound = this.audioContext.createBufferSource();
+        sound.connect(this.gainNode);
+        sound.buffer = this.bufferList[i];
+        sound.start(0);
+        sound.stop(this.audioContext.currentTime + 18);
+    }
 };
--- musicbox/BufferLoader.js
+++ musicbox/BufferLoader.js
@@ -1,49 +1,47 @@
-
 function BufferLoader(context, urlList, callback) {
   this.context = context;
   this.urlList = urlList;
   this.onload = callback;
-  this.bufferList = new Array();
+  this.bufferList = [];
   this.loadCount = 0;
 }
 
 BufferLoader.prototype.loadBuffer = function(url, index) {
   // Load buffer asynchronously
-  var request = new XMLHttpRequest();
+  const request = new XMLHttpRequest();
   request.open("GET", url, true);
   request.responseType = "arraybuffer";
 
-  var loader = this;
+  const loader = this;
 
-  request.onload = function() {
+  request.onload = () => {
     // Asynchronously decode the audio file data in request.response
     loader.context.decodeAudioData(
       request.response,
-      function(buffer) {
+      (buffer) => {
         if (!buffer) {
-          alert('error decoding file data: ' + url);
+          alert('Error Decoding File Data: ' + url);
           return;
         }
         loader.bufferList[index] = buffer;
         if (++loader.loadCount == loader.urlList.length)
           loader.onload(loader.bufferList);
       },
-      function(error) {
-        console.error('decodeAudioData error', error);
+      (error) => {
+        console.error('DecodeAudioData Error', error);
       }
     );
   };
 
-  request.onerror = function() {
-    alert('BufferLoader: XHR error');
+  request.onerror = () => {
+    alert('BufferLoader: XHR Error');
   };
 
   request.send();
 };
 
 BufferLoader.prototype.load = function() {
-	for (var i = 0; i < this.urlList.length; ++i) {
+	for (let i = 0; i < this.urlList.length; ++i) {
 		this.loadBuffer(this.urlList[i], i);
 	}
 };
-
